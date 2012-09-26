75fd9e20177eefb42dacd4119479e975574c1b78
Brett O'Donnell <cornernote@gmail.com>
2012-09-27 06:41:31 +0930

    fix lag, support multi-players, change nodes back

--- xray/init.lua
+++ xray/init.lua
@@ -1,15 +1,58 @@
-local function xray(mode)
-XRAY_MODE = mode
-end 
 
+-- expose api
+xray = {}
+
+-- the range of the xray effect
+xray.range = 10
+
+-- how long before the nodes turn back to stone
+xray.timer = 10
+
+-- mode is used to store the xray mode for each player
+xray.mode = {}
+
+-- should we spew out log messages?
+xray.debug = true
+ 
+-- log
+xray.log = function(message)
+	if not xray.debug then
+		return
+	end
+	minetest.log("action", "[xray] "..message)
+end
+ 
+-- replace stone with xray
+xray.replace = function(player_pos)
+	local pos = minetest.env:find_node_near(player_pos, xray.range, "default:stone")
+	while pos~= nil do
+		xray.log("replace default:stone with xray:stone at "..dump(pos))
+		minetest.env:add_node(pos,{name="xray:stone"})
+		minetest.after(xray.timer, xray.restore, pos)
+		pos = minetest.env:find_node_near(player_pos, xray.range, "default:stone")
+	end
+end
+
+-- restore xray to stone
+xray.restore = function(pos)
+	for _,object in ipairs(minetest.env:get_objects_inside_radius(pos, xray.range+2)) do
+		if object:is_player() and xray.mode[object:get_player_name()]==1 then
+			return
+		end
+	end
+	xray.log("restore xray:stone to default:stone at "..dump(pos))
+	minetest.env:add_node(pos,{name="default:stone"})
+end
+
+-- register_chatcommand
 minetest.register_chatcommand("xray", {
 	params = "<mode>",
 	description = "Make stone invisible.",
 	privs = {shout=true},	
 	func = function(name, param)
-		if param == 'on'  then xray(1)
+		if param == 'on'  then xray.mode[name]=1
 		  minetest.chat_send_player(name, "Xray turned on.")
-		elseif param == 'off' then xray(2)
+		elseif param == 'off' then xray.mode[name]=0
 		  minetest.chat_send_player(name, "Xray turned off.")
 		else
 		  minetest.chat_send_player(name, "Please enter 'on' or 'off'.")
@@ -17,25 +60,7 @@ minetest.register_chatcommand("xray", {
 	end,
 })
 
-minetest.register_abm({
-		nodenames = {"default:stone", "xray:stone"},
-		interval = 0,
-		chance = 4,	
-		
-		action = function(pos, node, active_object_count, active_object_count_wider)
-		  if XRAY_MODE == 1 then
-			  if node.name == "default:stone" then
-				  minetest.env:add_node(pos,{name="xray:stone"})
-		  elseif XRAY_MODE == 2 then
-		      if node.name == "xray:stone" then
-			      minetest.env:add_node(pos,{name="xray:stone"})
-	
-				end
-			end
-		end
-	end
-})
-
+-- register_node
 minetest.register_node("xray:stone", {
 	description = "Xray Stone",
 	tiles = {"xray_stone.png"},
@@ -46,3 +71,20 @@ minetest.register_node("xray:stone", {
 	sounds = default.node_sound_stone_defaults(),
 })
 
+-- register_globalstep - replace default:stone with xray:stone in range of players with xray
+minetest.register_globalstep(function(dtime)
+	for _,player in ipairs(minetest.get_connected_players()) do
+		local pos = player:getpos()
+		if xray.mode[player:get_player_name()] == 1 then
+			xray.replace(pos)
+		end
+	end
+end)
+
+-- register_abm - restore any stray xray:stone nodes to default:stone
+minetest.register_abm({
+    nodenames = {"xray:stone"},
+    interval = xray.timer,
+    chance = 1,
+    action = xray.restore,
+})
